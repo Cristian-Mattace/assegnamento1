1b8eca63fa4bf1c4490f12c8a20f2f946b2cc259
crist <cristianmattace5@gmail.com>
2021-10-27 16:09:19 +0200

    Main fatto bene

--- assegnamento1/src/assegnamento/Main.java
+++ assegnamento1/src/assegnamento/Main.java
@@ -5,13 +5,25 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Product cc = new Product("pippo",1,"rino", (float) 12.90);
+        Product sec = new Product("gbfv",2,"pino", (float) 12.90);
+        Product ter = new Product("heng",3,"gino", (float) 1);
+        Product qua = new Product("egb",4,"fino", (float) 12.90);
+        Product qui = new Product("qweafd",5,"sino", (float) 12.90);
+
+        ProductList pp = new ProductList();
+        pp.addProductList(cc);
+        pp.addProductList(sec);
+        pp.addProductList(ter);
+        pp.addProductList(qua);
+        pp.addProductList(qui);
+
         User uu = new User(0,"cc","c","x","cccc",false);
 
         System.out.println(cc.getName());
-        System.out.print("S");
-        System.out.println("U");
         System.out.println(uu.getId());
-        System.out.println(uu.getId());
-        System.out.println(uu.getId());
+
+        System.out.println(pp.getNumberOfElements());
+        Product p = pp.getProductMinPrice();
+        System.out.println(p.getCode());
     }
 }
